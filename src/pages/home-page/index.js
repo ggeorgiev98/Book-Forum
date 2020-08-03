d2424c6a4a38a605a145b2ad836b096597d3b6d5
georgiev98 <62682270+georgiev98@users.noreply.github.com>
2020-08-04 02:58:38 +0300

    Start the work
    Clear some useless stuff from react and initiate navigation + first home-page route.
    installed react-router-dom library

---
+++ src/pages/home-page/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+    constructor(props) {
+        super(props);
+
+    };
+
+    render() {
+        return (
+            <h1>We start the project!!!</h1>
+        );
+    };
+};
+
+export default HomePage;
