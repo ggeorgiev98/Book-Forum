33d4781636855aafaa6d62557175d612e9f8032f
georgiev98 <62682270+georgiev98@users.noreply.github.com>
2020-08-09 10:22:11 +0300

    Some css fixes
    Css fixes so the site became more responsive

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -5,7 +5,6 @@ import styles from './index.module.css';
 const Footer = () => {
     return (
         <footer className={styles.footer}>
-            <p className={styles.quote}>"A room without books is like a body without a soul."<br/>Cicero</p>
         </footer>
     );
 };
