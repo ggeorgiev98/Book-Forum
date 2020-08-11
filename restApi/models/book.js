6aa1e0b7c44501ceab011cd10c3d8eae90f410b0
georgiev98 <62682270+georgiev98@users.noreply.github.com>
2020-08-12 00:16:38 +0300

    added book and quote models + some files
    added book and quote mongoose models + some files

---
+++ restApi/models/book.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema ({
+    title: {
+        type: String,
+        required: true,
+        match: [/[A-Za-z0-9.,!?\s]+/g, 'Book name is not valid!'],
+        minlength: 3
+    },
+    description: {
+        type: String,
+        required: true,
+        maxlength: 500,
+        minlength: 30,
+        match: [/[A-Za-z0-9.,!?\s]+/g, 'Book description is not valid!']
+    },
+    imgUrl: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 20,
+        match: [/[A-Za-z0-9\s]+/g]
+    },
+    genre: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = new mongoose.model('Book', bookSchema);
---
+++ restApi/models/quote.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const quoteSchema = new mongoose.Schema ({
+    content: {
+        type: String,
+        required: true,
+        minlength: 30,
+        maxlength: 1000,
+        match: [/[A-Za-z0-9.,!?\s]+/g, "The content is invalid."]
+    },
+    author: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 20,
+        match: [/[A-Za-z0-9\s]+/g]
+    },
+    genre: {
+        type: String,
+        required: true
+    },
+    fromBook: {
+        required: true,
+        type: 'ObjectId',
+        ref: 'Book'
+    }
+});
